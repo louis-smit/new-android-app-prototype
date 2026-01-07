50959f25e8b6436c3defed16c06f4aa3bd8e1f51
Louis Smit <louissm.it@icloud.com>
2026-01-07 23:31:40 +0200

    Version 5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "no.solver.solverappdemo"
         minSdk = 24
         targetSdk = 35
-        versionCode = 4
+        versionCode = 5
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
